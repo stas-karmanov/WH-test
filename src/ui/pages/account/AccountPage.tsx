594fa3c7091a8560744d32d08924fd071b78a439
stanislav.karmanov <stanislav.karmanov@akveo.com>
2023-04-09 20:19:35 +0300

    prettify AccountPage

--- src/ui/pages/account/AccountPage.tsx
+++ src/ui/pages/account/AccountPage.tsx
@@ -3,30 +3,34 @@ import { Alert, Box, Snackbar, IconButton, Typography, Tooltip } from '@mui/mate
 import { Logout, Autorenew } from '@mui/icons-material';
 
 import { useAccountPageState } from './useAccountPageState';
+import { Secret } from '../../common/Secret';
 
 export const AccountPage: FC = () => {
   const { user, handleSecretRegeneration, handleLogout, error, setError } = useAccountPageState();
 
   return (
     <>
-      <Box>
-        {user && (
-          <>
-            <Typography>{user.secret}</Typography>
-            <Tooltip title="Regenerate secret">
-              <IconButton onClick={handleSecretRegeneration}>
-                <Autorenew />
-              </IconButton>
-            </Tooltip>
-          </>
-        )}
-
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <Box></Box>
+        <Typography variant="h5">Use or regenerate your secret</Typography>
         <Tooltip title="Logout">
           <IconButton onClick={handleLogout}>
             <Logout />
           </IconButton>
         </Tooltip>
       </Box>
+
+      {user && (
+        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mt: 1 }}>
+          <Secret secret={user.secret} />
+          <Tooltip title="Regenerate secret">
+            <IconButton onClick={handleSecretRegeneration}>
+              <Autorenew />
+            </IconButton>
+          </Tooltip>
+        </Box>
+      )}
+
       <Snackbar open={!!error} autoHideDuration={2000} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} onClose={() => setError('')}>
         <Alert severity="error">{error}</Alert>
       </Snackbar>
