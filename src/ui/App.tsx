5d4f34412362912e2be73c4f920efb120e30d5b6
stanislav.karmanov <stanislav.karmanov@akveo.com>
2023-04-09 20:35:18 +0300

    change subtitle2 color

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from 'react';
 import { ThemeProvider, createTheme, Theme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { Box } from '@mui/material';
 
 import { RouterOutlet } from './router/RouterOutlet';
 import { useInitialNavigation } from './useInitialNavigation';
-import { Box } from '@mui/material';
 
-const theme: Theme = createTheme({
+let theme: Theme = createTheme({
   components: {
     MuiCssBaseline: {
       styleOverrides: {
@@ -18,6 +18,18 @@ const theme: Theme = createTheme({
     },
   },
   palette: { mode: 'dark' },
+});
+
+theme = createTheme(theme, {
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        subtitle2: {
+          color: theme.palette.text.secondary,
+        },
+      },
+    },
+  },
 });
 
 export const App: FC = () => {
